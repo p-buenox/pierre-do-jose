9042cd00fb239a9c08c91468f30e65d583e563ba
pierre <pierre.bueno@escola.pr.gov.br>
2024-06-11 09:54:40 -0300

    conversoes

---
+++ conversoes.js
@@ -0,0 +1,21 @@
+// tipos dado 
+// booleanos
+
+// convesao implicita
+const numero = 456;
+const numerostring = "456";
+
+console.log(numero === numerostring);
+
+//conversao implicita
+// acontece quando o javascript converte o numero em string depois 
+// as 2 strings
+console.log(numero == numerostring);
+console,log(numero + numerostring);
+
+// conversao explicita 
+// number
+let novonumero = number(numerostring);
+console.log(numero +novonumero);
+
+//string
